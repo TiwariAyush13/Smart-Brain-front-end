7b55a433aeee4f362491d848ec6e93ed75227cfa
Ayush Tiwari <ayushonline.net@gmail.com>
2019-09-04 19:51:19 +0530

    front end for the face recognition

---
+++ src/Components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+    return(
+        <div >
+          <div className ='white f3 tc '>
+              {'Ayush,Your current rank is ...'}
+          </div>
+          <div className ='white f1 tc'>
+              {'#7'}
+          </div>
+        </div>
+    );
+ }
+
+export default Rank;
